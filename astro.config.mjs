7f983a82f4fad1f5c9a5f2659b599315fec426f5
rstringa <robertostringa@hotmail.com>
2024-08-15 19:00:30 -0300

    ....

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: false,
+  // prefetch: false,
   output: "server",
   adapter: vercel()
   // image: {
