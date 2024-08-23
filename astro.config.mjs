77b3f8872bd6dd18dacd42e7be8080d1d6e2dcc9
rstringa <robertostringa@hotmail.com>
2024-08-23 00:04:38 -0300

    data-astro-prefetch en links

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  // prefetch: false,
+  prefetch: true,
   output: "server",
   adapter: vercel()
   // image: {
